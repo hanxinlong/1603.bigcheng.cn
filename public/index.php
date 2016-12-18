cac5d27cd298863d75b8c2bbd96357d54c36a8bd
hanxinlong <1624495727@qq.com>
2016-12-18 20:50:31 +0800

    '22'

--- public/index.php
+++ public/index.php
@@ -12,9 +12,9 @@
 // [ 应用入口文件 ]
 
 // 定义应用目录
-//define('APP_PATH', __DIR__ . '/../application/');
+define('APP_PATH', __DIR__ . '/../application/');
 
 
 // 加载框架引导文件
-//require __DIR__ . '/../thinkphp/start.php';
- echo 123;
+require __DIR__ . '/../thinkphp/start.php';
+
